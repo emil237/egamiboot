4e4c67e2b92c8d5c138a5c4816fc6cd9eac783a1
emil237 <marianatalaat1@gmail.com>
2024-11-20 16:27:52 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 echo "install plugin egamiboot"
 cd /tmp
-curl  -k -Lbk -m 55532 -m 555104 "https://raw.githubusercontent.com/emil237/egamiboot/refs/heads/main/enigma2-plugin-extensions-egamiboot_10.5.ipk" > /tmp/enigma2-plugin-extensions-egamiboot_10.5.ipk
+curl  -k -Lbk -m 55532 -m 555104 "https://raw.githubusercontent.com/emil237/egamiboot/refs/heads/main/enigma2-plugin-extensions-egamiboot.ipk" > /tmp/enigma2-plugin-extensions-egamiboot.ipk
 sleep 1
 echo "install plugin...."
 cd /tmp
-opkg install /tmp/enigma2-plugin-extensions-egamiboot_10.5.ipk
+opkg install /tmp/enigma2-plugin-extensions-egamiboot.ipk
 echo ""
 echo ""
 echo ""
@@ -13,9 +13,21 @@ echo ""
 echo ""
 echo ""
 echo ""
+# Target directory
+TARGET_PATH="/usr/lib/enigma2/python/Plugins/Extensions/EGAMIBoot"
+
+# Check if the directory exists
+if [ -d "$TARGET_PATH" ]; then
+    echo "Changing permissions for: $TARGET_PATH"
+    chmod -R 755 "$TARGET_PATH"
+    echo "Permissions changed successfully."
+else
+    echo "The path $TARGET_PATH does not exist."
+    exit 1
+fi
 echo ""
 sleep 1
-rm /tmp/enigma2-plugin-extensions-egamiboot_10.5.ipk
+rm /tmp/enigma2-plugin-extensions-egamiboot.ipk
 sleep 2
 killall -9 enigma2
 exit
